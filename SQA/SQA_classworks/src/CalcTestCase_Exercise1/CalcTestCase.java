ff8cf1fba14ebd9dd76018afdea5ebfe381b8b94
ocean_shii <142196738+wintwah243@users.noreply.github.com>
2025-08-19 21:28:44 +0630

    exercise update :)

--- SQA/SQA_classworks/src/CalcTestCase_Exercise1/CalcTestCase.java
+++ SQA/SQA_classworks/src/CalcTestCase_Exercise1/CalcTestCase.java
@@ -5,35 +5,47 @@ import static org.junit.Assert.*;
 
 public class CalcTestCase {
 	
-	// test cases for addition
-	@Test
-	public void testAdd() {
-		// positive test cases for addition
+	@Test // positive test cases for addition 
+	public void testAddPositive() {
 		assertEquals("Addition should work by getting 5.", 5, Calc.add(2,3));
 		assertEquals("Addition should work by getting -3.", -3, Calc.add(-1, -2));
 	}
 	
-	// test cases for subtraction
-	@Test
-	public void testSubtract() {
-		// positive test cases for subtraction
+	
+	@Test // negative test case for addition
+	public void testAddNegative() {
+		assertNotEquals("Addition should not work.", 5, Calc.add(2, 2));
+	}
+	
+	
+	@Test // positive test cases for subtraction 
+	public void testSubtractPositive() {
 		assertEquals("Subtractions should work by getting 2.", 2, Calc.subtract(5, 3));
 		assertEquals("Subtractions should work by getting -2.", -2, Calc.subtract(3, 5));
 	}
 	
-	// test cases for multiplication
-	@Test
-	public void testMultiply() {
-		// positive test cases for multiplication
-		assertEquals("Multiplication should work by getting 8.", 8, Calc.multiply(4, 2));
-		assertEquals("Multiplication should work by getting -6", -6, Calc.multiply(2, -3));
-        assertEquals("Multiplication should wrok by getting 0", 0, Calc.multiply(0, 5));
+	
+	@Test // negative test case for subtraction
+	public void testSubtractNegative() {
+		assertNotEquals("Subtraction should not work.", 1, Calc.subtract(5, 5));
 	}
 	
-	// test cases for division
-	@Test
-	public void testDivision() {
-		// positive test cases for division
+	
+	@Test // positive test cases for multiplication 
+	public void testMultiplyPositive() {
+		assertEquals("Multiplication should work by getting 8.", 8, Calc.multiply(4, 2));
+		assertEquals("Multiplication should work by getting -6", -6, Calc.multiply(2, -3));
+	}
+	
+	
+	@Test // negative test case for multiplication
+	public void testMultiplyNegative() {
+		assertNotEquals("Multiplication should not work", 10, Calc.multiply(3, 3));
+	}
+	
+	
+	@Test // positive test cases for division
+	public void testDivisionPositive() {
 		assertEquals("Division should work by getting 2.", 2, Calc.divide(6, 3));
 		assertEquals("Division should work by getting -2.", -2, Calc.divide(6, -3));
 		
